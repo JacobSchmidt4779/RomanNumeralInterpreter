6818298d0dd08b6ba58ea9a29c7791f7f6be49fe
Jacob-DN-Schmidt <schjacob04@gmail.com>
2025-09-17 20:26:16 -0500

    Add project files.

---
+++ RomanNumeralInterpreter.cpp
@@ -0,0 +1,12 @@
+﻿// RomanNumeralInterpreter.cpp : Defines the entry point for the application.
+//
+
+#include "RomanNumeralInterpreter.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
