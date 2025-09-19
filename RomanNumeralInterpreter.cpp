31ee71e101ec6094dad37a0b45afcbc232629118
Dashiel2004 <106357778+Dashiel2004@users.noreply.github.com>
2025-09-19 14:41:09 -0500

    Update RomanNumeralInterpreter.cpp

--- RomanNumeralInterpreter.cpp
+++ RomanNumeralInterpreter.cpp
@@ -1,10 +1,42 @@
 ﻿#include "RomanNumeralInterpreter.h"
+#include <string>
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-	cout << "Hello CMake." << endl;
-	return 0;
+string romanConvert(int n) {
+
+    string roman[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+    int num[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+
+    string result = "";
+    int i = (sizeof(num)/sizeof(num[0])) - 1;
+
+
+
+    while (n > 0) {
+        int temp = n / num[i];
+        while (temp > 0) {
+            result += roman[i];
+            temp--;
+        }
+
+        n = n % num[i];
+
+        i--;
+    }
+
+    return result;
+}
+
+int main() {
+    cout << "Enter a number to be converted\n";
+    int num;
+    cin >> num;
+    if (num < 1 || num > 3999) {
+        cout << "invalid number\n";
+    }
+    else {
+        cout << romanConvert(num) + "\n";
+    }
+    return 0;
 }
