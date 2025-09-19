3c2b458b719890526a24b708c642fb4bbf5c0405
Jacob Schmidt <schmidtj4779@my.uwstout.edu>
2025-09-19 15:36:44 -0500

    Added integerConvert, added map for roman chars to int

--- RomanNumeralInterpreter.cpp
+++ RomanNumeralInterpreter.cpp
@@ -1,12 +1,18 @@
 ﻿#include "RomanNumeralInterpreter.h"
 #include <string>
 #include <iostream>
+#include <map>
+#include <functional>
 using namespace std;
+
+const string roman[] = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+const int num[] = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
+map<char, int> romanToInt = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
 
 string romanConvert(int n) {
 
-    string roman[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
-    int num[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+    /*string roman[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+    int num[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};*/
 
     string result = "";
     int i = (sizeof(num)/sizeof(num[0])) - 1;
@@ -28,7 +34,29 @@ string romanConvert(int n) {
     return result;
 }
 
+int integerConvert(string roman) {
+    int curr_digit = 0;
+    int res = 0;
+
+    for (int i = roman.size() - 1; i > -1; --i) {
+        if (auto search = romanToInt.find(roman.at(i)); search != romanToInt.end()) {
+            int curr_val = search->second;
+            if (curr_val >= curr_digit) {
+                curr_digit = curr_val;
+            }
+            else {
+                curr_val = -curr_val;
+            }
+            res += curr_val;
+        }
+    }
+    return res;
+}
+
 int main() {
+    int res = integerConvert("MMCDLXXXVII");
+    cout << res << "\n";
+    return 0;
     cout << "Enter a number to be converted\n";
     int num;
     cin >> num;
